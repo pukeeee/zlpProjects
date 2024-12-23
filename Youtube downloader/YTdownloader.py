5088a2567b2e1d30c5ca6d6584d19418d0ed7b7d
pukeeee <pukee18@ukr.net>
2024-12-23 17:16:47 +0200

    simplify

--- Youtube downloader/YTdownloader.py
+++ Youtube downloader/YTdownloader.py
@@ -3,27 +3,14 @@ import os
 
 def download_and_convert_video(url):
     ydl_opts = {
-        'format': 'bestvideo+bestaudio/best',
-        'outtmpl': os.path.join(os.path.dirname(__file__), '%(title)s.%(ext)s'),
-        'merge_output_format': 'mp4',
-        'postprocessors': [
+        "format": "bestvideo+bestaudio/best",
+        "outtmpl": os.path.join(os.path.dirname(__file__), "%(title)s.%(ext)s"),
+        "postprocessors": [
             {
-                'key': 'FFmpegVideoConvertor',
-                'preferedformat': 'mp4',
-            },
-            {
-                'key': 'FFmpegEmbedSubtitle',
-            },
-            {
-                'key': 'FFmpegVideoRemuxer',
-                'preferedformat': 'mp4',
-            },
-        ],
-        'postprocessor_args': [
-            '-c:v', 'libx264',
-            '-c:a', 'aac',
-            '-strict', 'experimental'
-        ],
+                "key" : "FFmpegVideoConvertor",
+                "preferedformat": "mp4"
+            }
+        ]
     }
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
