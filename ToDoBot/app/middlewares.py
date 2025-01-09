b2db173eb00da5e04add1e26a29a6203336ec6a7
pukeeee <pukee18@ukr.net>
2025-01-09 02:44:06 +0200

    none

--- ToDoBot/app/middlewares.py
+++
@@ -1,9 +0,0 @@
-from aiogram import BaseMiddleware
-from aiogram.types import Message, CallbackQuery
-
-class LanguageMiddleware(BaseMiddleware):
-    async def __call__(self, handler, event, data):
-        if isinstance(event, (Message, CallbackQuery)):
-            language_code = event.from_user.language_code or "en"
-            data["language_code"] = language_code
-        return await handler(event, data)
